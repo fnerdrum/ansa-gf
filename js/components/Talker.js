717a60f5afd67e818ece2debefe8449657fff0ce
Frithjof Nerdrum <frithjof.nerdrum@gmail.com>
2015-07-15 20:43:57 +0200

    Skiller på visning mellom innlegg og replikk

--- js/components/Talker.js
+++ js/components/Talker.js
@@ -4,13 +4,26 @@ import TalereStore from './../stores/TalereStore'
 class Talkers extends React.Component {
     render() {
         var talker = this.props.talker;
+        var className = 'list-group-item talker ' + getTypeNavn(talker.type);
         return (
-            <li className='list-group-item talker'>
+            <li className={className}>
                 <p className='number'>{talker.number}</p>
+
                 <p className='name'>{TalereStore.getParticipant(talker.number)}</p>
             </li>
         );
     }
 }
+
+const getTypeNavn = (type) => {
+    switch (type) {
+        case 'I':
+            return 'innlegg';
+        case 'R':
+            return 'replikk';
+        default :
+            return 'ukjent';
+    }
+};
 
 export default Talkers;
